97eb49b35a2918571db86a5a483244fd902f8811
Yuchen Zhou <yzh89@umd.edu>
2011-08-25 21:57:10 -0400

    Simulation Done

--- Markov/src/markov/TransitionMatrix.java
+++ Markov/src/markov/TransitionMatrix.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.List;
 
+
 /**
 * This class provides transition matrices with states indexed
 * from 0 to N-1.
@@ -21,7 +22,8 @@ public class TransitionMatrix<T extends Probability<T>> {
   public T get(int row, int col) {
     return data.get(row*N + col);
   }
-
+  
+  
   public static <T extends Probability<T>> Builder<T> create(int n) {
     return new Builder<T>(n);
   }
@@ -32,6 +34,63 @@ public class TransitionMatrix<T extends Probability<T>> {
 	public BuildException(String str) { super(str); }
   }
 
+  public static class RandomBuilder<T extends Probability<T>> {
+    public final int N;
+    ArrayList<ArrayList<T>> rows;
+
+    public RandomBuilder(int n) {
+      this.N = n;
+      rows = new ArrayList<ArrayList<T>>();
+      for (int i = 0; i < N; i++) {
+        rows.add(null);
+      }
+    }
+
+    public void set(int rowNum, T... row){
+      if (rows.size() > N) {
+        throw new BuildException(
+            "Attempted to add too many rows to TransitionMatrix.");
+      }
+      if (row.length != N) {
+        throw new BuildException("Wrong number of elements in row.");
+      }
+      if (rows.get(rowNum)!=null) rows.remove(rowNum);
+      rows.set(rowNum, new ArrayList<T>(Arrays.asList(row)));
+      
+    }
+
+    public void set(int rowNum, List<T> row){
+      if (rows.size() > N) {
+        throw new BuildException(
+            "Attempted to add too many rows to TransitionMatrix.");
+      }
+      if (row.size() != N) {
+        throw new BuildException("Wrong number of elements in row.");
+      }
+      if (rows.get(rowNum)!=null) rows.remove(rowNum);
+      rows.set(rowNum, new ArrayList<T>(row));
+      
+    }
+    public TransitionMatrix<T> build() {
+      if (rows.size() != N) {
+        throw new BuildException("Wrong number of rows n = "
+            + N + " != " + rows.size());
+      }
+      for (int i = 0; i < N; i++) {
+        ArrayList<T> row = rows.get(i);
+        if (row.size() != N) {
+          throw new BuildException("Row " + i + " has size "
+              + row.size() + " != " + N);
+        }
+      }
+      ArrayList<T> data = new ArrayList<T>();
+      for (ArrayList<T> row : rows) {
+        data.addAll(row);
+      }
+      return new TransitionMatrix<T>(N, data);
+    }
+    
+  }
   public static class Builder<T extends Probability<T>> {
     final int n;
     ArrayList<ArrayList<T>> rows;
@@ -40,6 +99,7 @@ public class TransitionMatrix<T extends Probability<T>> {
       this.n = n;
       rows = new ArrayList<ArrayList<T>>();
     }
+    
 
     public void addRow(T... row) {
       if (rows.size() >= n) {
@@ -51,6 +111,7 @@ public class TransitionMatrix<T extends Probability<T>> {
       }
       rows.add(new ArrayList<T>(Arrays.asList(row)));
     }
+
 
     public void addRow(List<T> row) {
       if (rows.size() >= n) {
@@ -84,5 +145,17 @@ public class TransitionMatrix<T extends Probability<T>> {
       }
       return new TransitionMatrix<T>(n, data);
     }
+  }
+  
+  public String toString(){
+    String out="";
+    for (int i=0; i<this.N;i++){
+      out=out+("[");
+      for (int j=0;j<this.N-1;j++){
+        out=out+" "+this.get(i, j)+",";
+      }
+      out=out+" "+this.get(i, this.N-1)+"]\n";
+    }
+    return out;
   }
 }
--- Markov/src/markov/TransitionVector.java
+++ Markov/src/markov/TransitionVector.java
@@ -101,8 +101,11 @@ public class TransitionVector<T extends Probability<T>>
     int myIdx = 0, idx = 0;
     while (myIdx < myNameArr.length && idx < nameArr.length) {
       int comp = myNameArr[myIdx].compareTo(nameArr[idx]);
-      if (comp == 0) throw new RuntimeException();
-      boolean nextSeq = comp < 0;
+      if (comp == 0){
+        
+        throw new RuntimeException();
+      }
+      boolean nextSeq = comp > 0;
       String next = nextSeq ? myNameArr[myIdx] : nameArr[idx];
       sequence[myIdx + idx] = nextSeq;
       if (nextSeq) myIdx++; else idx++;
