4ad19dd67b22717a8025902c2365cf9ad1c49590
Yuchen Zhou <yzh89@umd.edu>
2011-08-31 17:42:54 -0400

    This should be the correct one

--- Markov/src/markov/Simulation.java
+++ Markov/src/markov/Simulation.java
@@ -1,14 +1,20 @@
 package markov;
 
-// import java.util.ArrayList;
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.TreeMap;
+import java.util.Map.Entry;
 
 import parser.XmlParser;
+import util.Ptr;
 
 public class Simulation {
+  
+  public TreeMap<String,Integer> headlist;
+  public TransitionMatrix<FractionProbability> matrix;
   
   public static void main(String[] args) {
     System.out.println(new Simulation("xml/umlVersion2.xml"));
@@ -28,6 +34,103 @@ public class Simulation {
         stateNameList=initializeStates(tempA,tempB,stateNameList);
         tempA=tempB;
       }
+      
+      TransitionMatrix.RandomBuilder<FractionProbability> builder =new TransitionMatrix.RandomBuilder<FractionProbability>(stateNameList.size());
+
+      itr=net.iterator();
+      tempA=itr.next();
+      tempB=null;
+      while (itr.hasNext()){
+        tempB=itr.next();
+        String machineAName=tempA.name;
+        String machineBName=tempB.name;
+
+        
+        
+        
+        tempA=tempB;
+      }
+      
+      
+      String machineAName=tempA.name;
+      String machineBName=tempB.name;
+      Iterator<State<FractionProbability>> itrAState = tempA.iterator();
+      while (itrAState.hasNext()){
+        State<FractionProbability> Astate =itrAState.next();
+        DecisionTree<TransitionVector<FractionProbability>> decisionTreeA=Astate.getTransitionFunction();
+        Evaluation<TransitionVector<FractionProbability>> eva=new Evaluation<TransitionVector<FractionProbability>>(net.dictionary,decisionTreeA);
+        Evaluation<TransitionVector<FractionProbability>>.Evaluator root=eva.root;
+
+        
+        Iterator<State<FractionProbability>> itrBState= tempB.iterator();
+        while(itrBState.hasNext()){
+          State<FractionProbability> Bstate =itrBState.next();
+          Iterator<String> itrLabel=Bstate.labelNameIterator();
+          while(itrLabel.hasNext()){
+            String labelName=itrLabel.next();
+            String instance=Bstate.getLabel(labelName);
+            Predicate.Atom atom=new Predicate.Atom(machineBName,labelName,instance);
+            root=root.restrict(atom);      
+          }
+          
+          final Ptr<TransitionVector<FractionProbability>> ptrA=new Ptr<TransitionVector<FractionProbability>>();
+          root.accept(new Evaluation.Visitor<TransitionVector<FractionProbability>>() {
+
+            public void visitTerminal(Evaluation<TransitionVector<FractionProbability>>.Terminal term) {
+              ptrA.value=term.output;
+            }
+
+            public void visitWalker(Evaluation<TransitionVector<FractionProbability>>.Walker walker) {
+              ptrA.value=null;
+              System.err.println("End up in walker!!");
+            }
+          });
+          
+          Evaluation<TransitionVector<FractionProbability>>.Evaluator rootB=(new Evaluation<TransitionVector<FractionProbability>>(net.dictionary,Bstate.getTransitionFunction())).root;
+          itrLabel=Astate.labelNameIterator();
+          while(itrLabel.hasNext()){
+            String labelName=itrLabel.next();
+            String instance=Astate.getLabel(labelName);
+            Predicate.Atom atom=new Predicate.Atom(machineAName,labelName,instance);
+            rootB=rootB.restrict(atom);      
+          }
+          final Ptr<TransitionVector<FractionProbability>> ptrB=new Ptr<TransitionVector<FractionProbability>>();
+          rootB.accept(new Evaluation.Visitor<TransitionVector<FractionProbability>>() {
+
+            public void visitTerminal(Evaluation<TransitionVector<FractionProbability>>.Terminal term) {
+              ptrB.value=term.output;
+            }
+
+            public void visitWalker(Evaluation<TransitionVector<FractionProbability>>.Walker walker) {
+              ptrB.value=null;
+              System.err.println("End up in walker!!");
+            }
+          });
+          
+          int rowNum=stateNameList.get(Astate.name+Machine.MULTIPLY_STRING+Bstate.name);
+          TransitionVector<FractionProbability> out=ptrA.value.times(ptrB.value);
+          Iterator<Map.Entry<String, FractionProbability>> itrMergedLabel=out.iterator();
+          ArrayList<FractionProbability> row=new ArrayList<FractionProbability>(tempA.getStateNum()*tempB.getStateNum());
+          
+          for (int i=0;i<tempA.getStateNum()*tempB.getStateNum();i++){
+            row.add(i, FractionProbability.ZERO);
+          }
+          
+          while(itrMergedLabel.hasNext()){
+            Entry<String, FractionProbability> temp = itrMergedLabel.next();
+            int colNum=(stateNameList.get(temp.getKey())!=null)?stateNameList.get(temp.getKey()):-1;
+            if (colNum>-1) row.set(colNum, temp.getValue());
+          }
+          builder.set(rowNum, row);
+        }
+      }
+      this.matrix = builder.build();
+
+      
+      ValueComparator bvc =  new ValueComparator(stateNameList);
+      this.headlist = new TreeMap<String, Integer>(bvc);
+      headlist.putAll(stateNameList);
+      
       System.out.println(stateNameList.toString());
       
 
@@ -55,7 +158,7 @@ public class Simulation {
         }       
       }
     
-      return stateNameList;
+      return out;
     }
     else {
 
